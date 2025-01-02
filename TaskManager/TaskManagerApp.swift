61156455713ba2bf6c697f57268aa49a475aa1c4
David Calduch Terol <dacalte2012@gmail.com>
2025-01-02 17:58:49 +0100

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by David Calduch on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
