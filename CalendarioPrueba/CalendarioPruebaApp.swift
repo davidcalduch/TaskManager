41d323657b8259e6360cb2167d590970b0a8996b
David Calduch Terol <dacalte2012@gmail.com>
2025-02-09 11:14:57 +0100

    Initial Commit

---
+++ CalendarioPrueba/CalendarioPruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarioPruebaApp.swift
+//  CalendarioPrueba
+//
+//  Created by David Calduch on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendarioPruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarioPruebaTests/CalendarioPruebaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarioPruebaTests.swift
+//  CalendarioPruebaTests
+//
+//  Created by David Calduch on 9/2/25.
+//
+
+import Testing
+@testable import CalendarioPrueba
+
+struct CalendarioPruebaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
